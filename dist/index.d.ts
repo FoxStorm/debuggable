cfb288e453e59771570a16b3a546de887d3ae4fc
Sebastian Virlan <sebastian.virlan@bbc.co.uk>
2019-01-23 21:36:29 +0000

    Initial debuggable commit

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export { Debug } from './src/Debug';
+export { Debuggable } from './src/Debuggable';
+export { SourceLocation } from './src/SourceLocation';
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Debuggable_1 = require("./src/Debuggable");
+exports.Debuggable = Debuggable_1.Debuggable;
+var SourceLocation_1 = require("./src/SourceLocation");
+exports.SourceLocation = SourceLocation_1.SourceLocation;
+//# sourceMappingURL=index.js.map
